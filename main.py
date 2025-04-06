acc5a91fe464ae920a9abf030ce92a6f4741a86e
II10II <sonjachanga@gmail.com>
2025-04-06 22:59:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ img = load_image()
 
 result = st.button('Распознать изображение')
 if result:
-    captioner = pipeline("image-to-text","kha-white/manga-ocr-base")
+    captioner = pipeline("image-to-text","kazars24/trocr-base-handwritten-ru")
     text = captioner(img)
     st.write('**Результаты распознавания:**')
     st.write(text[0]["generated_text"])
